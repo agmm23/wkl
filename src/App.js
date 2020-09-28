641dce6a4e61c4398fc5d2af59fd9c4bd8842c7c
luisfreitasg <luisfreitasg@gmail.com>
2020-09-28 13:20:36 -0300

    Cambio en archivo App.js
    Se agregó Nacho y Alfonso

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Luis
+          Learn React Luis, Nacho, Alfonso
         </a>
       </header>
     </div>
